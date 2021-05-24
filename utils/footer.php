c91f9f8c448a2ae45932f28a509ff517ef09888c
Ramyaa S <ramyaasuresh01@gmail.com>
2021-05-24 17:00:36 +0530

    My First Commit

---
+++ utils/footer.php
@@ -0,0 +1,26 @@
+<hr class = "footerline"><!--css modified horizontal line-->
+<footer>
+    <div class = "container">
+        <div class = "row">
+            <section>
+                <div class = "footerContent col-md-4"><!--left content-->
+                    
+                    <p class = "footerSubtext2">
+                       Mahabinayak Puram, Patia Station Road, Bhubaneswar, India 751024.
+                        &copy; EvenzOne, 2020.
+                    </p>
+                </div>
+            </section>
+           
+            <section>
+                <div class = "footcontent col-md-4"><!--right content-->
+                    Follow Us:<br>
+                        <img src = "images/facebook.png">
+                        <img src = "images/twitter.png">
+                        <img src = "images/googleplus.png">
+                        <img src = "images/youtube.png">
+                </div>
+            </section>
+        </div>
+    </div>
+</footer>
